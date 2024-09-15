5d9047a2f9c53aad88654de0d25c0f24fa95b756
MBilalShaikh2003 <141023004+MBilalShaikh2003@users.noreply.github.com>
2024-09-15 14:00:16 +0500

    deploy error solve

--- src/app/page.js
+++ src/app/page.js
@@ -3,7 +3,7 @@ import Header from '@/components/Header';
 // import AuthForm from '@/components/AuthForm';
 import Body from '@/components/Body';
 import Footer from '@/components/Footer';
-import Modal from '@/components/Modal';
+// import Modal from '@/components/Modal';
 
 
 
